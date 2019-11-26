f83b65fda4d14c7e1d9bcc363f55d82627fd7c52
Fr0thgar <jona270r@edu.easj.dk>
2019-11-26 11:56:47 +0100

    Update WeatherStation.py

---
+++ WeatherStation.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from sense_hat import SenseHat
+sense = SenseHat()
+now = datetime.now()
+
+while True:
+    # Take readings from all three sensors
+    t = sense.get_temperature_from_humidity()
+    p = sense.get_pressure()
+    h = sense.get_humidity()
+
+    # Round the values to one decimal place
+    t = round(t, 1)
+    p = round(p, 1)
+    h = round(h, 1)
+    
+    message = "T: " + str(t) + " P: " + str(p) + " H: " + str(h) + " Time: " + str(now.strftime("%d/%m/%Y, %H:%M:%S"))
+    
+    sense.show_message(message, scroll_speed=0.05)
