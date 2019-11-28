0eecd22e2584551fda23e644eecb134d06c67266
Fr0thgar <jona270r@edu.easj.dk>
2019-11-28 11:53:31 +0100

    add rest

---
+++ WeatherLibrary/WeatherData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace WeatherLibrary
+{
+    public class WeatherData
+    {
+        public int _id;
+        public float _pressure;
+        public float _temperature;
+        public float _humidity;
+        public DateTime Time;
+
+        public WeatherData()
+        {
+        }
+
+        public WeatherData(int id, float pressure, float temperature, float humidity, DateTime time)
+        {
+            _id = id;
+            _pressure = pressure;
+            _temperature = temperature;
+            _humidity = humidity;
+            Time = time;
+        }
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public float Pressure
+        {
+            get => _pressure;
+            set => _pressure = value;
+        }
+
+        public float Temperature
+        {
+            get => _temperature;
+            set => _temperature = value;
+        }
+
+        public float Humidity
+        {
+            get => _humidity;
+            set => _humidity = value;
+        }
+
+        public DateTime Time1
+        {
+            get => Time;
+            set => Time = value;
+        }
+    }
+}
---
+++ WeatherStationExam/Controllers/WeatherDatasController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherStationExam.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherDatasController : ControllerBase
+    {
+        // GET: api/WeatherDatas
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/WeatherDatas/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/WeatherDatas
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/WeatherDatas/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
