217bc6dfd0b5a94aa0d648a9227d24d686feb16f
Oguz Dastan <oguzx@live.dk>
2019-12-03 11:03:37 +0100

    asdcasd

---
+++ WeatherStationExam/Managers/SQLConnectionStringSingleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherStationExam.Managers
+{
+    public class SQLConnectionSingleton
+    {
+        private static SQLConnectionSingleton _instance = new SQLConnectionSingleton();
+
+        public static SQLConnectionSingleton Instance => _instance;
+
+        private SQLConnectionSingleton()
+        {
+            _dbConnection = new SqlConnection(ConnString);
+            _dbConnection.Open();
+        }
+
+        private SqlConnection _dbConnection;
+        private const String ConnString = @"Server=tcp:saxildserver.database.windows.net,1433;Initial Catalog=SaxildDB;Persist Security Info=False;User ID=jon270r;Password=Qwerty12345!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public SqlConnection DbConnection => _dbConnection;
+    }
+}
