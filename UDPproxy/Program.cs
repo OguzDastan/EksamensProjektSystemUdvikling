102baa4f809b3e1a630f4c9a2d2ae7f8db100826
LarsDuJour <42436050+LarsDuJour@users.noreply.github.com>
2019-12-05 12:51:21 +0100

    UDP proxy påbegyndt

---
+++ UDPproxy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Sockets;
+
+
+namespace UDPproxy
+{
+    class Program
+    {
+        public static bool isRunning = false;
+        public static int listeningSocket = 6969;
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UDP Proxy says hello!");
+            Start();
+
+            while (isRunning)
+            {
+                //here we listen
+            }
+
+        }
+
+
+        static void Start()
+        {
+            UdpClient client = new UdpClient();
+            
+
+            isRunning = true;
+        }
+
+    }
+}
