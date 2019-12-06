caac74715224c8fa9c5c35d537ba782a4845e2ca
Oguz Dastan <oguzx@live.dk>
2019-12-06 15:08:55 +0100

    Vi tror proxy virker

--- UDPproxy/Program.cs
+++ UDPproxy/Program.cs
@@ -12,16 +12,33 @@ namespace UDPproxy
     class Program
     {
         public static bool isRunning = false;
-        public static int listeningSocket = 6969;
+        public const int listeningSocket = 6969;
 
 
 
         static void Main(string[] args)
         {
             Console.WriteLine("UDP Proxy says hello!");
-            UdpClient client = new UdpClient();
-            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, listeningSocket);
+            UdpClient client = new UdpClient(listeningSocket);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
 
+            /*
+            while (true)
+            {
+                byte[] bytes = client.Receive(ref endPoint);
+                string str = Encoding.UTF8.GetString(bytes);
+
+                Console.WriteLine("Modtaget " + str);
+                string upperStr = str.ToUpper();
+                byte[] buffer = Encoding.UTF8.GetBytes(upperStr.ToCharArray(), 0,
+                    upperStr.Length);
+
+                client.Send(buffer, buffer.Length, endPoint);
+
+            }
+            */
+
+            
             while (true)
             {
                 byte[] receiveBytes = client.Receive(ref endPoint);
@@ -33,6 +50,7 @@ namespace UDPproxy
                 rData = (formatter.Deserialize(receiveStream) as WeatherData);
                 Console.WriteLine(rData.ToString());
             }
+            
 
         }
 
